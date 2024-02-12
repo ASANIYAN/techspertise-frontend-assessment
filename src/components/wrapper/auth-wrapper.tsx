15e04d86cc82e83f876fe70973a6790121e8cdfb
ASANIYAN <ayomidepaulasaniyan@gmail.com>
2024-02-12 16:33:26 +0100

    fixed layout bug

--- src/components/wrapper/auth-wrapper.tsx
+++ src/components/wrapper/auth-wrapper.tsx
@@ -10,13 +10,13 @@ type AuthWrapperProps = {
 
 const AuthWrapper: React.FC<AuthWrapperProps> = ({ children }) => {
   return (
-    <section className="h-screen">
-      <section className="">
-        <section className="flex flex-nowrap w-full">
-          <section className="">
-            <SideBar />
-          </section>
-          <section className="w-full px-5 overflow-auto">{children}</section>
+    <section className="h-full">
+      <section className="flex h-screen">
+        <section className="h-full box-border">
+          <SideBar />
+        </section>
+        <section className="w-full px-5 box-border flex-1 overflow-auto">
+          {children}
         </section>
       </section>
     </section>
